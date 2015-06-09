d7aefc0826fdf4c0e6258c0cb2f7f74c6842b7ab
JonKale <jon@datasim.co.uk>
2015-06-09 19:26:32 +0100

    Added stubs

---
+++ LibJpegTurbo.Net/TurboJpegFile.cs
@@ -0,0 +1,19 @@
+﻿namespace LibJpegTurbo.Net
+{
+    using System;
+    using System.IO;
+
+    internal class TurboJpegFile
+    {
+        public static TurboJpegBuffer FromFile(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static TurboJpegBuffer FromStream(Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
